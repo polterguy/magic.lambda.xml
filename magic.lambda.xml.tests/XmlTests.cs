c799e0510ad0a0093b9429cd4f89db1529231487
Aista, Ltd <th@aista.com>
2022-11-27 12:11:24 +0200

    Better coverage

--- magic.lambda.xml.tests/XmlTests.cs
+++ magic.lambda.xml.tests/XmlTests.cs
@@ -29,5 +29,28 @@ xml2lambda:x:@.xml");
             Assert.Equal("Bloodroot", new Expression("**/xml2lambda/*/*/*/COMMON/*/\\#text").Evaluate(result).First().Value);
             Assert.Equal("qwertyuio", new Expression("**/xml2lambda/*/*/*/COMMON/*/\\@a").Evaluate(result).First().Value);
         }
+
+        [Fact]
+        public void Roundtrip()
+        {
+            Common.Evaluate(@"
+.xml:@""<CATALOG>
+  <PLANT>
+    <COMMON a=""""qwertyuio"""">Bloodroot</COMMON>
+    <BOTANICAL>Sanguinaria canadensis</BOTANICAL>
+    <ZONE>4</ZONE>
+    <LIGHT>Mostly Shady</LIGHT>
+    <PRICE>$2.44</PRICE>
+    <AVAILABILITY>031599</AVAILABILITY>
+  </PLANT>
+</CATALOG>""
+xml2lambda:x:@.xml
+lambda2xml:x:@xml2lambda/*
+if
+   neq:x:@lambda2xml
+      get-value:x:@.xml
+   .lambda
+      throw:Not matching");
+        }
     }
 }
